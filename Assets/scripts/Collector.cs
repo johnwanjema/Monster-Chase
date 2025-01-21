f139157f350c04206a43af7ee60b05aa599c04ae
Wanjema <jonwanjema@gmail.com>
2025-01-20 18:29:09 -0800

    clean up console

--- Assets/scripts/Collector.cs
+++ Assets/scripts/Collector.cs
@@ -4,16 +4,9 @@ public class Collector : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log($"Collision detected with: {collision.name}");
-
         if (collision.CompareTag("Enemy"))
         {
-            Debug.Log("Enemy destroyed!");
             Destroy(collision.gameObject);
-        }
-        else
-        {
-            Debug.Log("Not an enemy!");
         }
     }
 }
