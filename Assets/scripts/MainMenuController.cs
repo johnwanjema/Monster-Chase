e4de8c859d2b4c7da50e4958b19156448710edaa
Wanjema <jonwanjema@gmail.com>
2025-07-30 15:16:53 -0700

    add scene for main menu  and game manager script

--- Assets/scripts/MainMenuController.cs
+++ Assets/scripts/MainMenuController.cs
@@ -4,6 +4,12 @@ using UnityEngine.SceneManagement;
 public class MainmenuController : MonoBehaviour
 {
   public void PlayGame(){
+
+    // Returns a string
+    int selectedPlayer = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+    Debug.Log( "Index :" + selectedPlayer);
+
     SceneManager.LoadScene("Gameplay");
   }
 }
